b31fb44e41cc10ce60c148cec27c2de376b9010f
ShivamWho <shivam2094.be20@chitkara.edu.in>
2022-08-01 14:50:01 +0530

    Project Done

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Scroll to top button script
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function(){
+    window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+    });
+});
